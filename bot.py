5f485b1f9147eabd4d232ceba2be2e3890031802
seraphic <vqwt0143@gmail.com>
2025-12-03 20:29:09 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -18,19 +18,35 @@ class Cancel(discord.ui.View):
 @discord.ui.button(label="Confirm", style=discord.ButtonStyle.danger)
 async def confirm (self, i: discord.Interaction, b: discord.ui.Button):
   if i.user.id != self.ctx.author.id:
-    return await interaction.response.send_message(embed=discord.Embed(description=f"{i.user.mention}: You cannot respond to this 
-    
+    return await interaction.response.send_message(embed=discord.Embed(description=f"{i.user.mention}: You cannot respond to this interaction!", color=0x000001, ephemeral=True)
+
+  self.view.ctx = True
+  self.stop()
+
+@discord.ui.button(label="Cancel", style=discord.ButtonStyle.secondary)
+async def cancel(self, i: discord.Interaction, b: discord.ui.Button):
+  if i.user.id != self.ctx.author.id:
+    return await interaction.response.send_message(embed=discord.Embed(description=f"{i.user.mention}: You cannot respond to this interaction!", color=0x000001, ephemeral=True)
+  
+  self.view.ctx = False                                            
+  self.stop()
+                                                   
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def clean (ctx):
-  
+  await ctx.send(embed=discord.Embed(description="Are you sure? This will delete all emojis, stickers and roles from this server!", color=0xA4C4FF, view=Confirm(ctx), Cancel(ctx))
+    
+
 @bot.command()
 async def serverinfo (ctx):
   
   embed = discord.Embed(description=ctx.guild.name, color=0xA4C4FF)
   if ctx.guild.icon:
-    embed.set_thumbnail(url=ctx.guild.icon)
+    embed.set_thumbnail(url=ctx.guild.icon_url)
   embed.add_field(name="Owner: ", value=ctx.guild.owner, inline=True)
   embed.add_field(name="Boosts: ", value=ctx.guild
   .premium_subscription.count, inline=True)
-  embed.add_field(name="Boost Tier: ", value=
+  embed.add_field(name="Boost Tier: ", value=str(ctx.guild.premium_tier), inline=True)
+  embed.add_field(name="Channels: ", value=f"txt: {len(ctx.guild.text_channels)} | voice: {len(ctx.guild.voice_channels)}", inline=False)
+  await ctx.send(embed=embed)
+  
